2ba95027da70111d7251fe06933c8f6dd98de738
Chris Myers <cmyers2015@outlook.com>
2016-04-02 13:34:38 +0100

    add exception and error handling for user input

--- BattleshipsGame/Game.cs
+++ BattleshipsGame/Game.cs
@@ -19,8 +19,15 @@ namespace BattleshipsGame
 
         public bool StartGame()
         {
+            Console.WriteLine("Battleships! Alpha Build 0.1");
             Console.WriteLine("Please enter your name:");
-            player = new Player(Console.ReadLine());
+            string name = Console.ReadLine();
+            while(string.IsNullOrEmpty(name))
+            {
+                Console.WriteLine("Name can not be empty, please enter a name:");
+                name = Console.ReadLine();
+            }
+            player = new Player(name);
             player.CreateShip(ShipType.BATTLESHIP);
             player.CreateShip(ShipType.DESTROYER);
             player.CreateShip(ShipType.DESTROYER);
@@ -40,59 +47,70 @@ namespace BattleshipsGame
                 ConsoleDisplay.ShowPlayerBattleGrid(player);
                 Console.WriteLine("Select a Coordinate to Attack: ");
                 string coord = Console.ReadLine();
-                if(coord.Count() > 2 || !char.IsLetter(coord[0]) || !char.IsDigit(coord[1]) || string.IsNullOrEmpty(coord))
+                try
                 {
-                    Console.WriteLine("Invalid Coord. Example: A1");
-                    Console.ReadKey();
-                }
-                else
-                {
-                    y = int.Parse(coord[1].ToString().ToUpper());
-                    x = ConsoleDisplay.ALPHABET.IndexOf(coord[0].ToString().ToUpper());
-                    if (y > 9 || x > 9)
+                    if (string.IsNullOrEmpty(coord))
                     {
-                        Console.WriteLine("Coord outside boundaries. Please try again.");
-                        Console.ReadKey();
-                        continue;
+                        throw new ArgumentException();
                     }
-                    bool hit = player.Fire(x, y, cpu.PlayerGrid);
-                    if(hit)
+                    else if (coord.Count() > 2 || !char.IsLetter(coord[0]) || !char.IsDigit(coord[1]))
                     {
-                        Console.WriteLine("HIT!");
-                        if(player.BattleGrid.GetGridSquare(y, x).CheckStatus() == GridSquareStatus.SUNK)
-                        {
-                            Console.WriteLine("SUNK!");
-                        }
-                        Console.ReadKey();
+                        throw new ArgumentException();
                     }
                     else
                     {
-                        Console.WriteLine("MISS!");
-                        Console.ReadKey();
-                    }
-                    x = random.Next(0, 10);
-                    y = random.Next(0, 10);
-                    hit = cpu.Fire(x, y, player.PlayerGrid);
-                    if (hit)
-                    {
-                        Console.WriteLine("One of your Ships was HIT!");
-                        if (player.PlayerGrid.GetGridSquare(y, x).CheckStatus() == GridSquareStatus.SUNK)
+                        y = int.Parse(coord[1].ToString().ToUpper());
+                        x = ConsoleDisplay.ALPHABET.IndexOf(coord[0].ToString().ToUpper());
+                        if (y > 9 || x > 9)
                         {
-                            Console.WriteLine("One of your Ships SUNK!");
+                            Console.WriteLine("Coord outside boundaries. Please try again.");
+                            Console.ReadKey();
+                            continue;
                         }
-                        Console.ReadKey();
-                    }
+                        bool hit = player.Fire(x, y, cpu.PlayerGrid);
+                        if (hit)
+                        {
+                            Console.WriteLine("HIT!");
+                            if (player.BattleGrid.GetGridSquare(y, x).CheckStatus() == GridSquareStatus.SUNK)
+                            {
+                                Console.WriteLine("SUNK!");
+                            }
+                            Console.ReadKey();
+                        }
+                        else
+                        {
+                            Console.WriteLine("MISS!");
+                            Console.ReadKey();
+                        }
+                        x = random.Next(0, 10);
+                        y = random.Next(0, 10);
+                        hit = cpu.Fire(x, y, player.PlayerGrid);
+                        if (hit)
+                        {
+                            Console.WriteLine("One of your Ships was HIT!");
+                            if (player.PlayerGrid.GetGridSquare(y, x).CheckStatus() == GridSquareStatus.SUNK)
+                            {
+                                Console.WriteLine("One of your Ships SUNK!");
+                            }
+                            Console.ReadKey();
+                        }
 
-                    if (!player.CheckFleet())
-                    {
-                        Console.WriteLine("Game Over, " + cpu.Name + " Wins!");
-                        gameOver = true;
+                        if (!player.CheckFleet())
+                        {
+                            Console.WriteLine("Game Over, " + cpu.Name + " Wins!");
+                            gameOver = true;
+                        }
+                        if (!cpu.CheckFleet())
+                        {
+                            Console.WriteLine("Game Over, " + player.Name + " Wins!");
+                            gameOver = true;
+                        }
                     }
-                    if (!cpu.CheckFleet())
-                    {
-                        Console.WriteLine("Game Over, "+ player.Name+" Wins!");
-                        gameOver = true;
-                    }
+                }
+                catch(ArgumentException e)
+                {
+                    Console.WriteLine("Invalid Coord. Example: A1");
+                    Console.ReadKey();
                 }
             }
             return false;
